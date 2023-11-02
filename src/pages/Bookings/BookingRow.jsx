e388282e6c1f42c1c95b652239400d79ea4d9a85
Amin7775 <amin.dpi24@gmail.com>
2023-11-02 20:20:38 +0600

    update user using patch

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,5 +1,5 @@
-const BookingRow = ({ booking, handleDelete }) => {
-    const { _id, date, service, price, img } = booking;
+const BookingRow = ({ booking, handleDelete,handleBookingConfirm }) => {
+    const { _id, date, service, price, img, status } = booking;
 
     return (
         <tr>
@@ -21,7 +21,10 @@ const BookingRow = ({ booking, handleDelete }) => {
             <td>{date}</td>
             <td>${price}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                {   
+                    status ? <span className="text-primary font-bold">Confirmed</span>:
+                    <button onClick={()=>handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Confirm</button>
+                }
             </th>
         </tr>
     );
