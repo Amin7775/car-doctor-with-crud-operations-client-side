c5ad36b5cfe4bc105d8d74bb4c4e3ca80dd4a4bb
Amin7775 <amin.dpi24@gmail.com>
2023-11-03 22:12:36 +0600

    Access token refresh token and create simple access token

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -31,7 +31,7 @@ const routes = createBrowserRouter([
             },
             {
                 path:"/checkout/:id",
-                element: <BookService></BookService>,
+                element: <PrivateRoute><BookService></BookService></PrivateRoute>,
                 loader: ({params})=>fetch(`http://localhost:5000/services/${params.id}`)
             },
             {
